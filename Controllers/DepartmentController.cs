74922b570e75ed70fa7093a7f1865abf7dd41ed7
AdityaJoshiK <adityajoshi2107@gmail.com>
2023-09-02 23:26:36 +0530

    Department And Designation List Page DOne

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace Employee_Management.Controllers
+{
+
+    public class DepartmentController : Controller
+    {
+        private IConfiguration Configuration;
+        public DepartmentController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+
+        public IActionResult Index()
+        {
+            string str = this.Configuration.GetConnectionString("myConnectionString");
+            DataTable dt = new DataTable();
+            SqlConnection conn = new SqlConnection(str);
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "PR_MST_Department_SelectAll";
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            conn.Close();
+
+            return View("DepartmentList", dt);
+        }
+    }
+}
---
+++ Controllers/DesignationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Designation_Management.Controllers
+{
+    public class DesignationController : Controller
+    {
+        private IConfiguration Configuration;
+        public DesignationController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+        public IActionResult Index()
+        {
+            string str = this.Configuration.GetConnectionString("myConnectionString");
+            DataTable dt = new DataTable();
+            SqlConnection conn = new SqlConnection(str);
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "PR_MST_Designation_SelectAll";
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            conn.Close();
+
+            return View("DesignationList", dt);
+        }
+    }
+}
